8a00b743c72dd216fe165b5b9c9e7b658db13ff9
Jacob Morra <jacob.morra@uoit.net>
2016-01-06 16:47:38 -0500

    Create MathGameRunner.java

---
+++ math-game/MathGameRunner.java
@@ -0,0 +1,18 @@
+//*****************************************************
+//This program was created on Friday, November 27, 2015
+//by Jacob Morra
+//
+//The purpose of this class is to run the math game. It
+//holds the main class for starting the jar file.
+//******************************************************
+/** 
+ * This program runs the math game. 
+ */ 
+public class MathGameRunner { 
+	public static void main(String[] args) { 
+		//create new instance of MathGame called game
+		MathGame game = new MathGame(); 
+		game.play();//call play method on game (play game) 
+	} 
+}
+

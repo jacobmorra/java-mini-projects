329d9e1b181ec016e2c32656b0467913a979f54e
Jacob Morra <jacob.morra@uoit.net>
2016-01-24 02:34:59 -0500

    Create C-1-23_Tester.java

---
+++ data_structures-practice.java/C-1-23_Tester.java
@@ -0,0 +1,14 @@
+
+public class C_123Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {1,2,3};
+		int[] b = {20,40,60};
+		
+		System.out.println(C_123.dot(a, b));
+		
+		
+	}
+
+}
